7ee5d7f145eede66ede3e1793f972c99c7c43b41
YanVkhv <yan.vkhv@gmail.com>
2019-10-24 12:01:46 +0200

    Added test

--- domain/src/main/java/com/mavenmasters/digibooky/domain/db/BookDB.java
+++ domain/src/main/java/com/mavenmasters/digibooky/domain/db/BookDB.java
@@ -2,21 +2,19 @@ package com.mavenmasters.digibooky.domain.db;
 
 import com.mavenmasters.digibooky.domain.book.Author;
 import com.mavenmasters.digibooky.domain.book.Book;
-import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Repository
 public class BookDB implements Database {
 
-    private Map<String, Book> books;
+    private HashMap<String, Book> books;
 
     public BookDB() {
         this.books = this.generateBooks();
     }
 
-    private Map<String, Book> generateBooks() {
+    private HashMap<String, Book> generateBooks() {
         HashMap<String, Book> books = new HashMap<>();
         for (int i = 0; i < 10; i++) {
 
--- domain/src/test/java/com/mavenmasters/digibooky/domain/db/BookDBTest.java
+++ domain/src/test/java/com/mavenmasters/digibooky/domain/db/BookDBTest.java
@@ -1,17 +1,15 @@
 package com.mavenmasters.digibooky.domain.db;
 
-import org.junit.Before;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 class BookDBTest {
 
-    Database books;
+    Database books = new BookDB();
 
-    @Before
-    void setup() {
-        books = new BookDB();
+    @Test
+    void givenABookDB_whenCreated_thenContains10Books() {
+        Assertions.assertEquals(10, books.getAll().size());
     }
-
-
 }
