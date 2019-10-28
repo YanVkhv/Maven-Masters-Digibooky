14a52d9c8fdbda4d0f8243afc88fa9ab4a1d23ae
TomLeeBE <finalfantasy18@hotmail.com>
2019-10-28 08:51:53 +0100

    added securityconfig class, added admin functions to usercontroller class, updated domain-pom for security

--- service/src/main/java/com/mavenmasters/digibooky/service/AdminService.java
+++ service/src/main/java/com/mavenmasters/digibooky/service/AdminService.java
@@ -2,13 +2,14 @@ package com.mavenmasters.digibooky.service;
 
 import com.mavenmasters.digibooky.domain.db.EmployeeDb;
 import com.mavenmasters.digibooky.domain.users.employee.Admin;
-import com.mavenmasters.digibooky.domain.users.employee.Librarian;
 import com.mavenmasters.digibooky.domain.users.member.Email;
 import com.mavenmasters.digibooky.service.dto.CreateEmployeeDto;
 import com.mavenmasters.digibooky.service.dto.EmployeeDto;
 import com.mavenmasters.digibooky.service.dto.EmployeeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AdminService {
 
     private final EmployeeDb employees;
