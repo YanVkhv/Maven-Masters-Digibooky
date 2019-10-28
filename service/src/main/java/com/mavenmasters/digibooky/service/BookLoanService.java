51f40aa99796649179264a9e450236917b7e389b
YanVkhv <yan.vkhv@gmail.com>
2019-10-28 11:17:06 +0100

    Story 14

--- service/src/main/java/com/mavenmasters/digibooky/service/BookLoanService.java
+++ service/src/main/java/com/mavenmasters/digibooky/service/BookLoanService.java
@@ -7,6 +7,11 @@ import com.mavenmasters.digibooky.service.dto.BookLoanMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 @Service
 public class BookLoanService {
     private final BookLoanDB bookLoanDB;
@@ -22,5 +27,19 @@ public class BookLoanService {
 
     public BookLoanDto returnBookLoan(BookLoan bookLoan) {
         return BookLoanMapper.mapToDto(this.bookLoanDB.returnBookLoan(bookLoan.getId()));
+    }
+
+    public List<BookLoanDto> getAllBorrowedBooks(UUID id) {
+        return bookLoanDB.getAll().values().stream()
+                .filter(bookLoan -> bookLoan.getMemberId().equals(id))
+                .map(BookLoanMapper::mapToDto)
+                .collect(Collectors.toList());
+    }
+
+    public List<BookLoanDto> getAllOverdueBooks() {
+        return bookLoanDB.getAll().values().stream()
+                .filter(bookLoan -> bookLoan.getDueDate().isBefore(LocalDate.now()))
+                .map(BookLoanMapper::mapToDto)
+                .collect(Collectors.toList());
     }
 }
