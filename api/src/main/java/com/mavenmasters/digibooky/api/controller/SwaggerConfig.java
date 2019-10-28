1c212beb4beb3e83f50b644c946af455fbf85520
TomLeeBE <finalfantasy18@hotmail.com>
2019-10-28 14:52:11 +0100

    added swagger

--- api/src/main/java/com/mavenmasters/digibooky/api/controller/SwaggerConfig.java
+++ api/src/main/java/com/mavenmasters/digibooky/api/controller/SwaggerConfig.java
@@ -1,8 +1,9 @@
 package com.mavenmasters.digibooky.api.controller;
 
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger3
+@EnableSwagger2
 public class SwaggerConfig {
 }
